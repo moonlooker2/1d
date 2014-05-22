4fc37a1cd13f9f754b7af2ca1922cad6b1f6fbca
moonlooker2 <serpentediterra@libero.it>
2014-05-22 11:17:24 +0200

    Update json_napoligol1.js

--- assets/js/json_napoligol1.js
+++ assets/js/json_napoligol1.js
@@ -1,3 +1,58 @@
-$(function(){$.ajax({type:"GET",jsonp:"jsonp",url:"http://www.napoligol.it/news/Json_mrss1primopiano.php?callback=?",success:function(b){$.each(b.title,function(b,a){var c=a.locale,d=a.image,e=a.guid;$("#mrss1tutto").append('<div class="flashapp1"><img src='+d+'></><div class="titoloapp1"><a href='+e+"><h2>"+c+"</h2></a></div></div>")})}})});
-$(function(){$.ajax({type:"GET",jsonp:"jsonp",url:"http://www.napoligol.it/news/Json_mrss2tutto.php?callback=?",success:function(b){$.each(b.title,function(b,a){var c=a.locale,d=a.image,e=a.categoria,f=a.pubDate,g=a.guid;$("#mrss2tutto").append('<div class="flashapp"><img src='+d+'></><div class="titoloapp"><span class="data">'+f+"</span><a href="+g+"><h2>"+c+'</h2></a></div><div class="category">'+e+"</div></div>")})}})});
-$(function(){$.ajax({type:"GET",jsonp:"jsonp",url:"http://www.napoligol.it/news/Json_mrss3tutto.php?callback=?",success:function(b){$.each(b.title,function(b,a){var c=a.locale,d=a.image,e=a.categoria,f=a.pubDate,g=a.guid;$("#mrss3tutto").append('<div class="flashapp"><img src='+d+'></><div class="titoloapp"><span class="data">'+f+"</span><a href="+g+"><h2>"+c+'</h2></a></div><div class="category">'+e+"</div></div>")})}})});
+
+$(function() {
+	$.ajax({
+	type: "GET",
+    jsonp:'jsonp',
+	url : "http://www.napoligol.it/news/Json_mrss1primopiano.php?callback=?",
+	success: function (data) {
+$.each(data["title"], function(index, item) {
+						var $title = item.locale;
+						var $image = item.image;
+						var $guid = item.guid;
+						var $description = item.offerta;
+		  $("#mrss1tutto").append('<div class="flashapp1"><img class="lazy" src=' + $image + '></>' + '<div class="titoloapp1"><a href=' + $guid + '><h2>' + $title + '</h2></a></div></div>')
+						});
+				},
+      	});
+});
+
+$(function() {
+	$.ajax({
+	type: "GET",
+    jsonp:'jsonp',
+	url : "http://www.napoligol.it/news/Json_mrss2tutto.php?callback=?",
+	success: function (data) {
+$.each(data["title"], function(index, item) {
+						var $title = item.locale;
+						var $image = item.image;
+						var $category = item.categoria;
+						var $pubDate = item.pubDate;
+						var $guid = item.guid;
+						var $description = item.description;
+        $("#mrss2tutto").append('<div class="flashapp"><img class="lazy" src=' + $image + '></>' + '<div class="titoloapp"><span class="data">' + $pubDate + '</span><a href=' + $guid + '><h2>' + $title + '</h2></a></div><div class="category">' + $category + '</div></div>')
+						});
+				},
+      	});
+});
+
+
+$(function() {
+	$.ajax({
+	type: "GET",
+    jsonp:'jsonp',
+	url : "http://www.napoligol.it/news/Json_mrss3tutto.php?callback=?",
+	success: function (data) {
+$.each(data["title"], function(index, item) {
+						var $title = item.locale;
+						var $image = item.image;
+						var $category = item.categoria;
+						var $pubDate = item.pubDate;
+						var $guid = item.guid;
+						var $description = item.description;
+		  $("#mrss3tutto").append('<div class="flashapp"><img class="lazy" src=' + $image + '></>' + '<div class="titoloapp"><span class="data">' + $pubDate + '</span><a href=' + $guid + '><h2>' + $title + '</h2></a></div><div class="category">' + $category + '</div></div>')
+						});
+				},
+      	});
+});
+
+
